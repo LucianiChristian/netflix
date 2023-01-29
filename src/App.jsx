cdd2436b4b241972e7b9ffc3e9161737c726b814
Christian Luciani <luciani_christian@yahoo.com>
2023-01-29 10:27:57 -0600

    Initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import './App.css'
 
 function App() {
   return (
-    <h1>Clean</h1>
+    <div className="app">
+      
+    </div>
   )
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCL4Q9urj0fANTLAkvyAWOX2b3pH-hHKi8",
+    authDomain: "netflix-8f97d.firebaseapp.com",
+    projectId: "netflix-8f97d",
+    storageBucket: "netflix-8f97d.appspot.com",
+    messagingSenderId: "959189683159",
+    appId: "1:959189683159:web:671cf665554ebdbef4d992"
+};
