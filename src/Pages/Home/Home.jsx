7f0db1f8321f65493cfe5dde6320f41e21ee6cea
Christian Luciani <luciani_christian@yahoo.com>
2023-02-18 16:47:54 -0600

    Create navigation from "/" to "/login"

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,15 @@
 import "./Home.css";
 import netflixLogo from "./assets/netflix-logo.png";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 
 function Home() {
+    const navigate = useNavigate();
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        navigate("/login");
+    }
+
     return (  
         <div className="homeContainer">
             <div className="background">
@@ -10,17 +18,19 @@ function Home() {
             <div className="home">
                 <nav className="home__nav">
                         <img className="home__nav__logo" src={netflixLogo} alt="logo"/>
-                        <button className="home__nav__signInButton btn">Sign in</button>
+                        <Link to="/login">
+                            <button className="home__nav__signInButton btn">Sign in</button>
+                        </Link>
                 </nav>
                 <main className="home__body">
                     <h1>Unlimited movies, TV shows, and more.</h1>
                     <h2>Watch anywhere. Cancel anytime.</h2>
-                    <form className="home__form">
+                    <form className="home__form" onSubmit={ handleSubmit }>
                         <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
                         <div className="formControl">
-                            <input type="email" />
+                            <input type="email" required/>
                             <button>
-                                Get Started <i class="ri-arrow-right-s-line"></i>
+                                Get Started <i className="ri-arrow-right-s-line"></i>
                             </button>
                         </div>
                     </form>
